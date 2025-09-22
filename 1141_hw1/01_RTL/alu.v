c487c20e33ed12cffcaf92e378dd3093a62549d3
ricky980071 <ricky980071@gmail.com>
2025-09-22 10:12:24 +0800

    CVSD_first_commit

---
+++ 1141_hw1/01_RTL/alu.v
@@ -0,0 +1,20 @@
+module alu #(
+    parameter INST_W = 4,
+    parameter INT_W  = 6,
+    parameter FRAC_W = 10,
+    parameter DATA_W = INT_W + FRAC_W
+)(
+    input                      i_clk,
+    input                      i_rst_n,
+
+    input                      i_in_valid,
+    output                     o_busy,
+    input         [INST_W-1:0] i_inst,
+    input  signed [DATA_W-1:0] i_data_a,
+    input  signed [DATA_W-1:0] i_data_b,
+
+    output                     o_out_valid,
+    output        [DATA_W-1:0] o_data
+);
+
+endmodule
